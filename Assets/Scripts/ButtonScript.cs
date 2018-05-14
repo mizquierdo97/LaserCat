5e4f73925caaddd39d4f1401b20c0a25d098097b
mizquierdo97 <mizquierdo97@gmail.com>
2018-05-14 21:24:54 +0200

    Win Condition added

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,6 +6,7 @@ public class ButtonScript : MonoBehaviour {
 
     // Use this for initialization
     public GameObject door;
+    public GameObject manager;
     public bool activated = false;
 	void Start () {
 		
@@ -14,9 +15,13 @@ public class ButtonScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
         if (activated)
         {
-            door.GetComponent<DoorScript>().activated = true;
+            if(door != null)
+                door.GetComponent<DoorScript>().activated = true;
+            if (manager != null)
+                manager.GetComponent<WinScript>().actual_buttons_pressed++;
         }
 
         activated = false;
---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour {
+
+    public int win_number;
+    public int actual_buttons_pressed = 0;
+    public string change_scene;
+	// Use this for initialization
+	void Start () {
+        StopAllCoroutines();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(actual_buttons_pressed >= win_number)
+        {
+            StopAllCoroutines();
+            SceneManager.LoadScene(change_scene, LoadSceneMode.Single);
+        }
+
+        actual_buttons_pressed = 0;
+	}
+}
