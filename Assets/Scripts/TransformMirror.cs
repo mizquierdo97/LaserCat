e2ad44217369997e53eb14772c1184b58bf49533
mizquierdo97 <mizquierdo97@gmail.com>
2018-05-15 00:40:40 +0200

    Bug fixed

---
+++ Assets/Scripts/TransformMirror.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMirror : MonoBehaviour {
+
+    public GameObject Map;
+    public float Offset = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       if( transform.position.y <  (Map.transform.position.y + Offset) - 0.2f)
+        transform.localPosition = new Vector3(0, transform.localPosition.y+0.05f, 0);
+       else if (transform.position.y > (Map.transform.position.y + Offset) + 0.2f)
+            transform.localPosition = new Vector3(0, transform.localPosition.y - 0.05f, 0);
+        //transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
+    }
+}
---
+++ Assets/Scripts/TransformRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation= Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
+
+        int x = 0;
+    }
+}
