518e428ee4cf51a6d15ceffb40348b9d56a1ceb7
mizquierdo97 <mizquierdo97@gmail.com>
2018-05-15 08:59:07 +0200

    Created particles

---
+++ Assets/Scripts/ActivateParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticle : MonoBehaviour {
+
+	public int particle_num;
+	public WinScript win;
+
+	ParticleSystem emitter;
+	// Use this for initialization
+	void Start () {
+		emitter = GetComponent<ParticleSystem> ();
+		emitter.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (win.actual_buttons_pressed >= particle_num) {
+			emitter.Play ();
+		}
+	}
+}
