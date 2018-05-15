29838c00c9b18bb86e7bf5cad7d53f0d132748df
mizquierdo97 <mizquierdo97@gmail.com>
2018-05-15 09:39:35 +0200

    Added UI

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+
+	Text text;
+	public WinScript manager;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = manager.actual_buttons_pressed.ToString() + " / " + manager.win_number.ToString ();
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+
+	Text timer;
+	float miliseconds = 0;
+	// Use this for initialization
+	void Start () {
+		timer = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		miliseconds += Time.deltaTime * 100;
+		miliseconds = miliseconds % 100;
+		timer.text = (((int)Time.timeSinceLevelLoad)%60).ToString () + " : " + ((int)miliseconds).ToString ();
+	}
+}
